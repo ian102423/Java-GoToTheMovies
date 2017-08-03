0e7ad8a359b882caaa5f0b1f804bab098a5fabdc
Ian Lee <ian102423@github.com>
2017-08-02 22:57:59 -0500

    Initial Commit

---
+++ src/main/java/com/theironyard/GO_TO_THE_MOVIES/GoToTheMoviesApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard.GO_TO_THE_MOVIES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoToTheMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoToTheMoviesApplication.class, args);
+	}
+}
---
+++ src/main/java/com/theironyard/GO_TO_THE_MOVIES/ResultPage.java
@@ -0,0 +1,25 @@
+package com.theironyard.GO_TO_THE_MOVIES;
+
+import java.util.List;
+
+public class ResultPage {
+
+    List<Movie> results;
+
+    ResultPage() {
+
+    }
+
+    public ResultPage(List<Movie> results) {
+        this.results = results;
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+
+}
